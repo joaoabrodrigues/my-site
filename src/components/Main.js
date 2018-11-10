2067d615a01f831dc9f964229a091b8a9615e21e
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-11-09 23:57:51 -0200

    update site

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 
@@ -12,55 +11,56 @@ class Main extends React.Component {
 
     return (
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+      
+        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">About</h2>
+          <span className="image main"><img src={pic03} alt="yeah, that's me!" /></span>
+          <p>My name is João, I'm around here since 1993.</p>
+          <p>I was born in Nova Londrina - state of Paraná - and now I'm living in Maringá.</p>
+          <p>I have Bachelor's degree in Information Systems (IT) by UNIPAR - Universidade Paranaense from Paranavaí - state of Paraná -
+             Graduate Degree in App Development for Mobile Devices and Graduate Degree in WebMobile Development and Technologies, both by FCV - Faculdade Cidade Verde.</p>
+          <p>Also, I'm currently studying Graduate Degree in Object Oriented Development in Java by UNICIV.</p>
+          <p>I started my professional carreer right after finish the college, in January 2015. Now, I work as Software Engineer at DB1 Global Software.</p>
           {close}
         </article>
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
-          {close}
-        </article>
-
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p>Here, there are some of my skills and what I work with.</p>
+          <ul>
+            <li>Java <span className="right">&#9733;&#9733;&#9733;&#9733;&#9733;</span></li>
+            <li>Spring (Framework, Boot, Data, Cloud, Security)<span className="right">&#9733;&#9733;&#9733;&#9733;</span></li>
+            <li>Git <span className="right">&#9733;&#9733;&#9733;&#9733;</span></li>
+            <li>Scrum<span className="right">&#9733;&#9733;&#9733;&#9733;</span></li>
+            <li>Eclipse<span className="right">&#9733;&#9733;&#9733;&#9733;</span></li>
+            <li>JUnit, Mockito, Powermock<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>JEE<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>Hibernate<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>JSF, JSP, Servlets<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>SQL<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>Javascript<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>HTML<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>TDD<span className="right">&#9733;&#9733;&#9733;</span></li>
+            <li>CSS<span className="right">&#9733;&#9733;</span></li>
+            <li>IntelliJ IDEA<span className="right">&#9733;&#9733;</span></li>
+            <li>Nodejs<span className="right">&#9733;&#9733;</span></li>
+            <li>React, React Native<span className="right">&#9733;&#9733;</span></li>
+            <li>Angular 4+<span className="right">&#9733;&#9733;</span></li>
+            <li>NoSQL (MongoDB)<span className="right">&#9733;&#9733;</span></li>
+          </ul>
           {close}
         </article>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
+          <p>Feel free to contact me at <a href="mailto:contato@joaoabrodrigues.com">contato@joaoabrodrigues.com</a> or any social network below.</p>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="http://www.linkedin.com/in/joaoabrodrigues" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+            <li><a href="http://www.github.com/joaoabrodrigues" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="http://www.facebook.com/joaoabrodrigues" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+            <li><a href="http://www.instagram.com/joaoabrodrigues" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+            <li><a href="http://www.twitter.com/joaoabrodrigues" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
           </ul>
           {close}
         </article>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
