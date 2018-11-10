37bccab1fb31c061a3b644e2c12edd5f8ee4d91d
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-11-10 00:02:55 -0200

    adding docs

---
+++ docs/component---src-pages-404-js-123f41acfadb49c70969.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{139:function(e,t,n){"use strict";n.r(t);var a=n(0),o=n.n(a),u=n(144),l=n(143);t.default=function(){return o.a.createElement(u.a,null,o.a.createElement("h1",null,"NOT FOUND"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-123f41acfadb49c70969.js.map
