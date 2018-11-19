0faeb14ec64b4b45a8cf65eb9e0afb0e6733af67
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-11-19 21:10:12 -0200

    changing background

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'joaoabrodrigues.com',
+        short_name: 'jaber',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        background_color: '#837E87',
+        theme_color: '#000000',
+        display: 'standalone',
         icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
