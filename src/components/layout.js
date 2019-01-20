ef588a71544b895af253a6577be61bbe089009cb
joaoabrodrigues <joaoabrodrigues@outlook.com>
2019-01-19 23:13:33 -0200

    editing description

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Read my bio, my skills and more!' },
+              { name: 'keywords', content: 'bio, developer, java, spring' },
             ]}
           >
             <html lang="en" />
